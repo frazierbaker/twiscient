267b55c5294ff43b202517e74805532b5e3e10c0
Frazier Baker <frazierbaker@users.noreply.github.com>
2017-12-06 23:23:50 -0500

    Adding provisioning scripts

---
+++ setup/install_spark.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Installing spark on Frazier's Avatar Cluster
+################################################
+
+# Run on Alpine 3.6
+
+# install base packages
+apk --update add bash curl util-linux coreutils binutils findutils grep procps openjdk8-jre
+
+# download spark
+curl -o /spark.tgz http://mirrors.ibiblio.org/apache/spark/spark-2.2.0/spark-2.2.0-bin-hadoop2.7.tgz
+
+# move spark to spark folder
+mv ./spark-* /spark
